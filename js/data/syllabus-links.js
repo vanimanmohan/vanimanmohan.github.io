73577cb686c13acbfd58126a3a19bc99a321b371
Dr. Vani Priya <42743726+vanimanmohan@users.noreply.github.com>
2018-12-25 16:39:43 +0530

    Update syllabus-links.js

--- js/data/syllabus-links.js
+++ js/data/syllabus-links.js
@@ -1,8 +1,7 @@
 var syllabus_data = {
   "Links": [
         { 
-            "Title": "Python e-books", 
-       
+            "Title": "Python e-books",
             "URL": "https://drive.google.com/file/d/1_-TkTF1QJD8o7wBDDBPM4NqHeEvW1mPq/view?usp=sharing",
             "Type": "e-book" 
         },
