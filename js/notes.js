2abe933c3a3cd6ad77cb86599a83003ab95c5df9
Dr. Vani Priya <42743726+vanimanmohan@users.noreply.github.com>
2019-04-07 12:29:03 +0530

    Update notes.js

--- js/notes.js
+++ js/notes.js
@@ -3,7 +3,7 @@ for (var i = 0; i < notes_data.Links.length; i++) {
                 "<tr> \
                     <td align='center' width='4%'><b>"+ (i + 1) + "." + "</b></td> \
                     <td align='left' ><a href='"+ notes_data.Links[i].URL + "'>" + notes_data.Links[i].Title + "</a></td> \
-                    <td align='right' width='10%'>"+ notes_data.Links[i].Type + "</td> \
+                    <td align='right' width='4%'>"+ notes_data.Links[i].Type + "</td> \
                 </tr>"
     document.getElementById("table-body").innerHTML += html;
 
