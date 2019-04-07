8d40f3654f0040e721b0588e120991dbb4edccf5
Dr. Vani Priya <42743726+vanimanmohan@users.noreply.github.com>
2019-04-07 21:59:48 +0530

    Update syllabus.js

--- js/syllabus.js
+++ js/syllabus.js
@@ -3,7 +3,7 @@ for (var i = 0; i < syllabus_data.Links.length; i++) {
                 "<tr> \
                     <td align='center' width='4%'><b>"+ (i + 1) + "." + "</b></td> \
                     <td align='left' ><a href='"+ syllabus_data.Links[i].URL + "'>" + syllabus_data.Links[i].Title + "</a></td> \
-                    <td align='right' width='10%'>"+ syllabus_data.Links[i].Type + "</td> \
+                    <td align='left' width='20%'>"+ syllabus_data.Links[i].Type + "</td> \
                 </tr>"
     document.getElementById("table-body").innerHTML += html;
 
